068d0696e6bc8dd2c67037d091d35b90cf8f3f61
Gumbraise <34947108+Gumbraise@users.noreply.github.com>
2026-06-08 15:43:59 +0200

    feat(controllers): add front controllers for PS8 and PS9

---
+++ controllers/front/cron.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once _PS_MODULE_DIR_ . 'mailjet/classes/endpoints/run.php';
+
+class MailjetCronModuleFrontController extends ModuleFrontController
+{
+    public $ssl = true;
+    public $display_header = false;
+    public $display_footer = false;
+
+    public function postProcess()
+    {
+        MailjetEndpointRunner::run('cron');
+    }
+}
